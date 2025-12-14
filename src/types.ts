d68604c9c15d05cb7fc1a12467f163cb2eeca8bf
Alex Schneider <me@schneider.ax>
2025-12-14 19:50:21 +0700

    Define base types

---
+++ src/types.ts
@@ -0,0 +1,234 @@
+// For reference, see https://github.com/OHF-Voice/wyoming.
+// The description there is incomplete and somewhat incorrect though.
+
+interface Attribution {
+  name: string
+  url: string
+}
+
+interface Artifact {
+  name: string
+  attribution: Attribution
+  installed: boolean
+  description?: string
+  version?: string
+}
+
+interface Model extends Artifact {
+  languages: string[]
+}
+
+interface AudioFormat {
+  rate: number
+  width: number
+  channels: number
+}
+
+interface Voice {
+  name?: string
+  language?: string
+  speaker?: string
+}
+
+type EventBody<
+  TData extends object | undefined = undefined,
+  TPayload extends Buffer | undefined = undefined,
+> = (undefined extends TData
+  ? {
+      data?: TData
+    }
+  : {
+      data: TData
+    }) &
+  (undefined extends TPayload
+    ? {
+        payload?: TPayload
+      }
+    : {
+        payload: TPayload
+      })
+
+type EventBodies = {
+  // Audio - Send raw audio and indicate begin/end of audio streams
+  'audio-chunk': EventBody<{ timestamp?: number } & AudioFormat, Buffer>
+  'audio-start': EventBody<{ timestamp?: number } & AudioFormat>
+  'audio-stop': EventBody<{ timestamp?: number }>
+
+  // Info - Describe available services
+  describe: EventBody
+  info: EventBody<{
+    asr?: (Artifact & { models: Model[]; supports_transcript_streaming: boolean })[]
+    tts?: (Artifact & { models: Model[]; supports_synthesize_streaming: boolean })[]
+    wake?: (Artifact & { models: Model[] })[]
+    handle?: (Artifact & { models: Model[]; supports_handled_streaming: boolean })[]
+    intent?: (Artifact & { models: Model[] })[]
+    satellite?: {
+      area?: string
+      has_vad?: boolean
+      active_wake_words?: string[]
+      max_active_wake_words?: number
+      supports_trigger: boolean
+    }
+    mic?: { mic_format: AudioFormat }[]
+    snd?: { snd_format: AudioFormat }[]
+  }>
+
+  // Speech Recognition - Transcribe audio into text
+  /** Request to transcribe an audio stream */
+  transcribe: EventBody<{
+    name?: string
+    language?: string
+    context?: object
+  }>
+  /** Response with transcription */
+  transcript: EventBody<{
+    text: string
+    language?: string
+    context?: object
+  }>
+  'transcript-start': EventBody<{
+    context?: object
+  }>
+  'transcript-chunk': EventBody<{
+    text: string
+  }>
+  'transcript-stop': EventBody
+
+  // Text to Speech - Synthesize audio from text
+  synthesize: EventBody<{
+    text: string
+    voice?: Voice
+  }>
+  'synthesize-start': EventBody<{
+    voice?: Voice
+    context?: object
+  }>
+  'synthesize-chunk': EventBody<{
+    text: string
+  }>
+  'synthesize-stop': EventBody
+  'synthesize-stopped': EventBody
+
+  // Wake Word - Detect wake words in an audio stream
+  detect: EventBody<{
+    names?: string[]
+  }>
+  detection: EventBody<{
+    name?: number
+  }>
+  'not-detected': EventBody
+
+  // Voice Activity Detection - Detects speech and silence in an audio stream
+  'voice-started': EventBody<{
+    timestamp?: number
+  }>
+  'voice-stopped': EventBody<{
+    timestamp?: number
+  }>
+
+  // Intent Recognition
+  recognize: EventBody<{
+    text: string
+    context?: object
+  }>
+  intent: EventBody<{
+    name: string
+    text?: string
+    context?: object
+    entities: { name: string; value?: unknown }[]
+  }>
+  'not-recognized': EventBody<{
+    text?: string
+    context?: object
+  }>
+
+  // Intent Handling - Handle structured intents or text directly
+  handled: EventBody<{
+    text?: string
+    context?: object
+  }>
+  'not-handled': EventBody<{
+    text?: string
+    context?: object
+  }>
+  'handled-start': EventBody<{
+    context?: object
+  }>
+  'handled-chunk': EventBody<{
+    text: string
+  }>
+  'handled-stop': EventBody
+
+  // Audio Output - Play audio stream
+  played: EventBody
+
+  // Voice Satellite
+  'run-satellite': EventBody
+  'pause-satellite': EventBody
+  'satellite-connected': EventBody
+  'satellite-disconnected': EventBody
+  'streaming-started': EventBody
+  'streaming-stopped': EventBody
+  /** Pipelines are run on the server, but can be triggered remotely from the server as well */
+  'run-pipeline': EventBody<{
+    start_stage: string
+    end_stage: string
+    /** From client only */
+    wake_word_name?: string
+    /** From server only */
+    wake_word_names?: string[]
+    /** From server only */
+    announce_text?: string
+    /** Only used for always-on streaming satellites */
+    restart_on_end?: boolean
+  }>
+
+  // Timers
+  'timer-started': EventBody<{
+    id: string
+    total_seconds: number
+    name?: string
+    start_hours?: number
+    start_minutes?: number
+    start_seconds?: number
+    command?: string
+    text: string
+    language?: string
+  }>
+  'timer-updated': EventBody<{
+    id: string
+    is_active: boolean
+    total_seconds: number
+  }>
+  'timer-cancelled': EventBody<{
+    id: string
+  }>
+  'timer-finished': EventBody<{
+    id: string
+  }>
+
+  // Misc
+  ping: EventBody<{
+    text?: string
+  }>
+  pong: EventBody<{
+    text?: string
+  }>
+}
+
+type EventHeader<TType extends keyof EventBodies = keyof EventBodies> = {
+  type: TType
+  version?: string
+  data_length?: number
+  payload_length?: number
+}
+
+type Events = {
+  [K in keyof EventBodies]: EventBodies[K] & {
+    header: EventHeader<K>
+  }
+}
+
+type Event<TType extends keyof EventBodies = keyof EventBodies> = Events[TType]
+
+export type { EventBody, EventHeader, Events, Event }
