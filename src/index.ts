f03ba369e3d2c0db4faebd4089ee22d546596699
Alex Schneider <me@schneider.ax>
2025-12-16 22:14:24 +0700

    Add exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Adapter from './Adapter.js'
+import Connectable from './Connectable.js'
+import Server from './Server.js'
+
+export { Connectable, Adapter, Server }
+export * from './events.js'
---
+++ src/tcp.ts
@@ -0,0 +1,2 @@
+export { default as TcpClientAdapter } from './TcpClientAdapter.js'
+export { default as TcpServerAdapter } from './TcpServerAdapter.js'
